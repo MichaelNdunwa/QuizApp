401c0f8e081b0b1657e9d073dbd1cf188d39748b
michael <ndunwa240@gmail.com>
2024-01-07 17:03:32 +0100

    Learning how to build note app

--- app/src/main/java/com/michael/quizapp/ui/QuestionsActivity.kt
+++ app/src/main/java/com/michael/quizapp/ui/QuestionsActivity.kt
@@ -1,10 +1,10 @@
 package com.michael.quizapp.ui
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
@@ -16,9 +16,12 @@ import com.michael.quizapp.utils.Constants
 class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityQuestionsBinding
-    private val currentPosition = 1
+    private var questionsCounter = 0
     private lateinit var questionsList: MutableList<Question>
     private var selectedOptionPosition = 0
+    private var selectedAnswer = 0
+    private lateinit var currentQuestion: Question
+    private var answered = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,23 +36,45 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
         binding.checkButton.setOnClickListener(this)
 
         questionsList = Constants.getQuestions()
-        setQuestion()
+        showNextQuestion()
     }
 
-    private fun setQuestion() {
-        val question = questionsList[currentPosition]
+    //    private fun showNextQuestion() {
+    private fun showNextQuestion() {
+        resetOptions()
+        val question = questionsList[questionsCounter]
         binding.flagImage.setImageResource(question.image)
-        binding.progressBar.progress = currentPosition
-        binding.textViewProgress.text = "$currentPosition/${binding.progressBar.max}"
+        binding.progressBar.progress = questionsCounter
+        binding.textViewProgress.text = "${questionsCounter + 1}/${binding.progressBar.max}"
         binding.textViewOptionOne.text = question.optionOne
         binding.textViewOptionTwo.text = question.optionTwo
         binding.textViewOptionThree.text = question.optionThree
         binding.textViewOptionFour.text = question.optionFour
 
-        when(currentPosition) {
-            questionsList.size -> binding.checkButton.text = "FINISH"
-            else -> binding.checkButton.text = "CHECK"
+        // Change progress bar color:
+        binding.progressBar.progressDrawable.setTintList(ColorStateList.valueOf(Color.BLACK))
+
+
+        /*   if (questionsCounter < questionsList.size) {
+               binding.checkButton.text = "CHECK"
+               currentQuestion = questionsList[questionsCounter]
+           } else {
+               binding.checkButton.text = "FINISH"
+           }*/
+
+        when {
+            questionsCounter < questionsList.size -> {
+                binding.checkButton.text = "CHECK"
+                currentQuestion = questionsList[questionsCounter]
+            }
+
+            else -> {
+                binding.checkButton.text = "FINISH"
+            }
         }
+
+        questionsCounter++
+        answered = false
 
     }
 
@@ -69,10 +94,50 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.text_view_option_one -> {
+                selectedOption(binding.textViewOptionOne, 1)
+            }
+
+            R.id.text_view_option_two -> {
+                selectedOption(binding.textViewOptionTwo, 2)
+            }
+
+            R.id.text_view_option_three -> {
+                selectedOption(binding.textViewOptionThree, 3)
+            }
+
+            R.id.text_view_option_four -> {
+                selectedOption(binding.textViewOptionFour, 4)
+            }
+
+            R.id.check_button -> {
+                if (!answered) {
+                    checkAnswer()
+                } else {
+                    //showNextQuestion()
+                    showNextQuestion()
+                }
+
+                /*if (answered) {
+                   // showNextQuestion()
+                    setQuestion()
+                } else {
+                    checkAnswer()
+                }*/
+
+                selectedAnswer = 0
+            }
+
+        }
+    }
+
+
     private fun selectedOption(textView: TextView, selectedOptionNumber: Int) {
         resetOptions()
-
-        selectedOptionPosition = selectedOptionNumber
+//        selectedOptionPosition = selectedOptionNumber
+        selectedAnswer = selectedOptionNumber // my correction to the code.
         textView.setTextColor(Color.parseColor("#363A43"))
         textView.setTypeface(textView.typeface, Typeface.BOLD)
         textView.background = ContextCompat.getDrawable(
@@ -80,24 +145,76 @@ class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
         )
     }
 
+    private fun checkAnswer() {
+        answered = true
 
+        if (selectedAnswer == currentQuestion.correctAnswer) {
+            highlightCorrectAnswer(selectedAnswer)
+        } else {
+            highlightWrongAnswer(selectedAnswer)
+        }
 
-    override fun onClick(view: View?) {
-        when(view?.id) {
-            R.id.text_view_option_one -> {
-                selectedOption(binding.textViewOptionOne, 1)
-            }
-            R.id.text_view_option_two -> {
-                selectedOption(binding.textViewOptionTwo, 2)
-            }
-            R.id.text_view_option_three -> {
-                selectedOption(binding.textViewOptionThree, 3)
-            }
-            R.id.text_view_option_four -> {
-                selectedOption(binding.textViewOptionFour, 4)
-            }
-            R.id.check_button -> {
+        binding.checkButton.text = "NEXT"
+        showSolution()
+    }
 
+    private fun showSolution() {
+        selectedAnswer = currentQuestion.correctAnswer
+        highlightCorrectAnswer(selectedAnswer)
+    }
+
+    private fun highlightCorrectAnswer(answer: Int) {
+        when (answer) {
+            1 -> {
+                binding.textViewOptionOne.background = ContextCompat.getDrawable(
+                    this, R.drawable.correct_option_border_bg
+                )
+            }
+
+            2 -> {
+                binding.textViewOptionTwo.background = ContextCompat.getDrawable(
+                    this, R.drawable.correct_option_border_bg
+                )
+            }
+
+            3 -> {
+                binding.textViewOptionThree.background = ContextCompat.getDrawable(
+                    this, R.drawable.correct_option_border_bg
+                )
+            }
+
+            4 -> {
+                binding.textViewOptionFour.background = ContextCompat.getDrawable(
+                    this, R.drawable.correct_option_border_bg
+                )
+            }
+        }
+    }
+
+    private fun highlightWrongAnswer(answer: Int) {
+        when (selectedAnswer) {
+            1 -> {
+                binding.textViewOptionOne.background = ContextCompat.getDrawable(
+                    this, R.drawable.wrong_option_border_bg
+                )
+            }
+
+            2 -> {
+                binding.textViewOptionTwo.background = ContextCompat.getDrawable(
+                    this, R.drawable.wrong_option_border_bg
+                )
+            }
+
+            3 -> {
+                binding.textViewOptionThree.background = ContextCompat.getDrawable(
+                    this, R.drawable.wrong_option_border_bg
+                )
+            }
+
+            4 -> {
+                binding.textViewOptionFour.background = ContextCompat.getDrawable(
+                    this, R.drawable.wrong_option_border_bg
+                )
             }
         }
     }
---
+++ app/src/main/java/com/michael/quizapp/ui/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.michael.quizapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.michael.quizapp.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
